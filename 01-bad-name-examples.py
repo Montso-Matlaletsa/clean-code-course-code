8b896651ccb7a508be0a200ccfc5ab247854f437
Montso-Matlaletsa <90146972+Montso-Matlaletsa@users.noreply.github.com>
2022-01-31 22:02:13 -0800

    Update 01-bad-name-examples.py

--- 01-bad-name-examples.py
+++ 01-bad-name-examples.py
@@ -1,24 +1,24 @@
 from datetime import datetime
 
 
-class Entity:
-    def __init__(self, title, description, ymdhm):
+class BlogPost:
+    def __init__(self, title, description, date):
         self.title = title
         self.description = description
-        self.ymdhm = ymdhm
+        self.date = date
 
 
-def output(item):
+def printPost(item):
     print('Title: ' + item.title)
     print('Description: ' + item.description)
-    print('Published: ' + item.ymdhm)
+    print('Published: ' + item.date)
 
 
 summary = 'Clean Code Is Great!'
-desc = 'Actually, writing Clean Code can be pretty fun. You\'ll see!'
+description = 'Actually, writing Clean Code can be pretty fun. You\'ll see!'
 new_date = datetime.now()
 publish = new_date.strftime('%Y-%m-%d %H:%M')
 
-item = Entity(summary, desc, publish)
+post = BlogPost(summary, desc, publish)
 
-output(item)
+printPost(post)
