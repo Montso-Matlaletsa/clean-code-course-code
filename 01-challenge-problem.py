987748b1548cfc906f9f0efdf4c7a9324bec1bfb
Montso-Matlaletsa <90146972+Montso-Matlaletsa@users.noreply.github.com>
2022-01-31 21:58:35 -0800

    cleans variable names

--- 01-challenge-problem.py
+++ 01-challenge-problem.py
@@ -1,35 +1,35 @@
 class Point:
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX
-        self.coordY = coordY
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class Rectangle:
-    def __init__(self, starting_point, broad, high):
+    def __init__(self, starting_point, width, height):
         self.starting_point = starting_point
-        self.broad = broad
-        self.high = high
+        self.width = width
+        self.height = height
 
-    def area(self):
-        return self.broad * self.high
+    def getArea(self):
+        return self.width * self.height
 
-    def end_points(self):
-        top_right = self.starting_point.coordX + self.broad
-        bottom_left = self.starting_point.coordY + self.high
-        print('Starting Point (X)): ' + str(self.starting_point.coordX))
-        print('Starting Point (Y)): ' + str(self.starting_point.coordY))
+    def print_coordinates(self):
+        top_right = self.starting_point.x + self.width
+        bottom_left = self.starting_point.y + self.height
+        print('Starting Point (X)): ' + str(self.starting_point.x))
+        print('Starting Point (Y)): ' + str(self.starting_point.y))
         print('End Point X-Axis (Top Right): ' + str(top_right))
         print('End Point Y-Axis (Bottom Left): ' + str(bottom_left))
 
 
-def build_stuff():
-    main_point = Point(50, 100)
-    rect = Rectangle(main_point, 90, 10)
+def createRectangle():
+    rectangle_starting_point = Point(50, 100)
+    rectangle = Rectangle(rectangle_starting_point , 90, 10)
 
-    return rect
+    return rectangle
 
 
-my_rect = build_stuff()
+rectangle =createRectangle()
 
-print(my_rect.area())
-my_rect.end_points()
+print(rectangle.getArea())
+rectangle.print_coordinates()
